73d68aada1c5aa02a300fda0c4f000f3a3d70b49
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2024-05-05 00:45:22 -0700

    Rectified LoginPage.jsx and HomePage.jsx

--- MailFend/Frontend/src/Components/HomePage.jsx
+++ MailFend/Frontend/src/Components/HomePage.jsx
@@ -1,23 +1,15 @@
-import api from "../api";
+// import api from "../api";
 import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
   const navigate = useNavigate();
 
-  const endMailConnection = async () => {
-    try {
-      const res = await api.post("api/logout/", { logout: true });
-      console.log(res.data.message);
-    } catch (error) {
-      console.log(`${error.response.data.message}`);
-    }
-  };
-
-  async function handleLogout() {
+  function handleLogout() {
     localStorage.clear();
-    await endMailConnection();
     navigate("/login");
+    console.log("User successfully logged out.");
   }
+
   return (
     <div>
       <h1>This is the home page.</h1>
--- MailFend/Frontend/src/Components/LoginPage.jsx
+++ MailFend/Frontend/src/Components/LoginPage.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import { TextField, Button, IconButton, InputAdornment } from '@mui/material';
-import { Visibility, VisibilityOff, Mail, Close } from '@mui/icons-material'; // Import the Close icon
-import logo from '../images/logo.png';
-import api from '../api';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { TextField, Button, IconButton, InputAdornment } from "@mui/material";
+import { Visibility, VisibilityOff, Mail, Close } from "@mui/icons-material"; // Import the Close icon
+import logo from "../images/logo.png";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../tokens";
 
 const LoginPage = ({ handleClose }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
@@ -19,27 +20,88 @@ const LoginPage = ({ handleClose }) => {
     e.preventDefault();
   };
 
+  const getFakeUsername = (email_address) => {
+    email_address = email.split("@");
+    const username = email_address[0];
+    return username;
+  };
+
+  const createUser = async (e) => {
+    const username = getFakeUsername(email);
+
+    try {
+      const res = await api.post("api/create-user/", {
+        username,
+        email,
+        password,
+      });
+
+      if (res.status === 201 && res.data.exists === true) {
+        console.log("This user already has a registered account.");
+      }
+    } catch (error) {
+      console.log(
+        `ERROR FROM CREATE USER FUNCTION -> ${JSON.stringify(
+          error.response.data
+        )}`
+      );
+    }
+  };
+
+  const setTokens = async (e) => {
+    const username = getFakeUsername(email);
+
+    try {
+      const res = await api.post("api/token/", { username, password });
+      localStorage.setItem(ACCESS_TOKEN, res.data.access);
+      localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+    } catch (error) {
+      console.log(`ERROR FROM SET TOKENS FUNCTION -> ${error}`);
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const res = await api.post('api/login/', { email, password });
+      const res = await api.post("api/login/", { email, password });
       if (res.status === 200) {
-        navigate('/home');
+        await createUser();
+
+        await setTokens();
+
+        console.log("Login successful.");
+
+        navigate("/home");
       }
     } catch (error) {
-      console.log(error);
+      const status_code = error.response.status;
+      const error_code = error.response.data.error_code;
+
+      if (status_code === 400 && error_code === "002")
+        console.log("Invalid username or password.");
+      if (status_code === 500) {
+        console.log("The connection with the server failed.");
+      }
+      if (status_code === 503) {
+        console.log(
+          "Unable to establish a stable internet connection. Please check your network connection and try again."
+        );
+      }
     }
   };
 
   return (
-    <div className="  modal-container login-form bg-white p-8 rounded-md shadow-lg "> 
+    <div className="  modal-container login-form bg-white p-8 rounded-md shadow-lg ">
       <div className="flex text-red-600 justify-end">
         <IconButton onClick={handleClose}>
           <Close />
         </IconButton>
       </div>
-      <form className="flex flex-col border items-center justify-center w-full" onSubmit={handleSubmit}>
+      <form
+        className="flex flex-col border items-center justify-center w-full"
+        onSubmit={handleSubmit}
+      >
         <div className="flex flex-col py-2 px-4 items-center justify-center w-full">
           <h1 className="text-black text-center flex text-xl items-center">
             MailFend
@@ -70,7 +132,7 @@ const LoginPage = ({ handleClose }) => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
-            type={showPassword ? 'text' : 'password'}
+            type={showPassword ? "text" : "password"}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
